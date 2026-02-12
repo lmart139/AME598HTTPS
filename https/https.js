6466241a17a74e456be92f383fd99179bb335024
Lily Martin <111933085+lmart139@users.noreply.github.com>
2026-02-11 18:42:53 -0700

    https

---
+++ https/https.js
@@ -0,0 +1,17 @@
+var homeDir = require('path').join(require('os').homedir());
+require('greenlock-express').create({
+  version: 'draft-11'
+, server: 'https://acme-v02.api.letsencrypt.org/directory'
+//, server: 'https://acme-staging-v02.api.letsencrypt.org/directory'  // staging
+, email: 'lmart139@asu.edu'                                     // CHANGE THIS
+, agreeTos: true
+, approveDomains: [ 'lily.httpsexample.xyz', 'www.lily.httpsexample.xyz' ]              // CHANGE THIS
+, store: require('greenlock-store-fs')
+, configDir: homeDir
+//, app: require('express')().use('/', function (req, res) {
+//    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+//    res.end('Hello, World!\n\n💚 🔒.js');
+//  })
+, app: require('./server.js')
+//, communityMember: true
+}).listen(8080, 8443);
